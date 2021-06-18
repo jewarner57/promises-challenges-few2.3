4d18a7c517e2e77c39fc05b4d6524d0ca2aa3ca1
Jonathan Warner <jewarner57@gmail.com>
2021-06-18 09:54:44 -0400

    challenge 1 complete

--- Promise-1/index.js
+++ Promise-1/index.js
@@ -2,7 +2,10 @@
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     // reject("--- Oops ---");
+    reject('Failure')
+    reject('Failure')
     resolve('>>> Success! <<<');
+
   }, 2000);
 });
 
@@ -17,9 +20,10 @@ p.then((message) => {
 // **Problems to solve**
 
 // **1)** What happens when a promise is rejected? Test it by calling `reject()`
-
+// It logs 'Promise rejected'
 // **2)** What happens when you call both `resolve` and `reject`? Test this.
-
+// The promise is resolved
 // **3)** Does the order matter you call resolve and reject matter? Test this. 
-
+// Yes it does
 // **4)** What happens if you call `resolve` or `reject` more than once? Test this out for yourself.
+// It only fails once
