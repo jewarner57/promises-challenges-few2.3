5ece0809156967fb15f8bf6dd95b5d21b37cd5bb
Jonathan Warner <jewarner57@gmail.com>
2021-06-18 10:02:22 -0400

    challenge 5 complete

--- Promise-5/index.js
+++ Promise-5/index.js
@@ -1,12 +1,21 @@
 function whatToEat(timeOfDay) {
   // Create a new promise and return it here
-  
+  return new Promise((resolve, reject) => {
+    if (timeOfDay === 'breakfast' || timeOfDay === 'lunch' || timeOfDay === 'dinner') {
+      resolve('macaroni')
+    }
+    reject('I am not hungry right now')
+  })
 }
 
 
 // Call yout function and resolve the promise here! 
-whatToEat('lunch')
-
+whatToEat('lunch').then((f) => {
+  console.log(f)
+})
+  .catch((err) => {
+    console.log(err)
+  })
 
 // If the `timeOfDay` is 'breakfast', 'lunch', or 'dinner' 
 // resolve the promise with your favorite food for that time. 
